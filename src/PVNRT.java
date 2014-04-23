92e6744996c5f30e8fc124cb0bccdfdd7d0b0e1d
Lyrositor <gagne.marc@gmail.com>
2014-04-23 15:02:29 +0200

    Génération de particules.

--- src/PVNRT.java
+++ src/PVNRT.java
@@ -7,14 +7,14 @@ import java.math.*;
 public class PVNRT {
 
     // Paramètres des particules.
-    static final double MASSE_PARTICULE = 1.0;
-    static final double NOMBRE_PARTICULES = 100;
+    static final double MASSE_PARTICULE = 40.0;
+    static final int NOMBRE_PARTICULES = 100;
     static final double VITESSE_INITIALE_PARTICULE = 1.0;
 
     // Paramètres de la fenêtre de simulation.
-    static final int CHAMBRE_WIDTH = 100;
-    static final int CHAMBRE_HEIGHT = 100;
-    static final int LARGEUR_MIN = 20;
+    static final int CHAMBRE_LARGEUR = 100;
+    static final int CHAMBRE_HAUTEUR = 100;
+    static final int HAUTEUR_MIN = 20;
     static final int DELAI = 10;
     static final String WIN_TITRE = "Simulateur PVNRT - Groupe B5-2";
     static final double DELTA_T_SIMULATION = 0.2;
@@ -24,15 +24,9 @@ public class PVNRT {
     static final BigDecimal kB = new BigDecimal("1.38E-16");
     static final double R = 8.314;
 
-    // Différentes formules disponibles.
-    enum Formule {
-        Euler,
-        Verlet
-    }
-
     // Variables globales.
     static double[][] fParticules;
-    static Formule fFormule = Formule.Euler;
+    static int[][] fCouleurs;
 
     /**
      * Initialise la simulation.
@@ -40,10 +34,20 @@ public class PVNRT {
      */
     public static void main(String[] args) {
         // Initialise les particules.
+        fParticules = new double[NOMBRE_PARTICULES][6];
+        fCouleurs = new int[NOMBRE_PARTICULES][3];
+        double vitesse_max = 0;
+        for (int i = 0; i < NOMBRE_PARTICULES; i++) {
+            fParticules[i] = creerParticule();
+            vitesse_max = Math.max(vitesse_max, calculVitesseParticule(fParticules[i]));
+        }
+        for (int i = 0; i < NOMBRE_PARTICULES; i++)
+            fCouleurs[i] = creerCouleurParticule(fParticules[i], vitesse_max);
 
         // Initialiser et afficher la fenêtre.
-	    Affichage.initAffichage(CHAMBRE_WIDTH, CHAMBRE_HEIGHT, LARGEUR_MIN,
+	    Affichage.initAffichage(CHAMBRE_LARGEUR, CHAMBRE_HAUTEUR, HAUTEUR_MIN,
 	        DELAI, WIN_TITRE, DELTA_T_SIMULATION, PISTON_VARIATION_MODULE);
+        Affichage.setParticules(fParticules, fCouleurs);
     }
 
     /**
@@ -53,15 +57,40 @@ public class PVNRT {
      * choisi aléatoirement dans [0; 2π[.
      */
     public static double[] creerParticule() {
-        double[] particule = {0, 0, 0, 0};
+        double theta = Math.random() * 2 * Math.PI;
+        double[] particule = {
+            randomInRange(0, CHAMBRE_LARGEUR),
+            randomInRange(0, HAUTEUR_MIN),
+            VITESSE_INITIALE_PARTICULE * Math.cos(theta),
+            VITESSE_INITIALE_PARTICULE * Math.sin(theta),
+            0,
+            0};
         return particule;
+    }
+
+    /**
+     * Renvoie un tableau RGB de couleur représentatif de la vitesse.
+     */
+    public static int[] creerCouleurParticule(double[] particule, double vitesse_max) {
+        double rapport = calculVitesseParticule(particule) / vitesse_max;
+        int[] couleur = {(int) (rapport * 255), 0, 0};
+        return couleur;
+    }
+
+    /**
+     * Renvoie la norme de la vitesse d'une particule (m.s^-1).
+     */
+    public static double calculVitesseParticule(double[] particule) {
+        double vitesse = Math.sqrt(Math.pow(particule[2], 2) + Math.pow(particule[3], 2));
+        return vitesse;
     }
 
     /**
      * Renvoie la norme de la force appliquée sur une particule (N).
      */
-    public static double calculForceParticule() {
-        return 0.0;
+    public static double[] calculForceParticule(double[] particule) {
+        double[] force = {0.0, 0.0};
+        return force;
     }
 
     /**
@@ -117,9 +146,15 @@ public class PVNRT {
      * Appelé lorsque la fenêtre rafraîchit l'affichage.
      * Déplace les particules et actualise les graphiques.
      */
-    public static void next(){
+    public static void next() {
         
     }
 
+    /**
+     * Calcule un int aléatoire entre deux valeurs minimales et maximales.
+     */
+    public static int randomInRange(int min, int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
 
 }
