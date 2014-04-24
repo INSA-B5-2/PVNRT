2e3e238b6f7f5cdffc580448160ed17b100b9ed1
Lyrositor <gagne.marc@gmail.com>
2014-04-24 10:22:34 +0200

    Couleurs, piston

--- src/PVNRT.java
+++ src/PVNRT.java
@@ -4,25 +4,25 @@
 
 public class PVNRT {
 
-    // Paramètres des particules.
-    static final double MASSE_PARTICULE = 1.0;
-    static final int NOMBRE_PARTICULES = 100;
-    static final double VITESSE_INITIALE_PARTICULE = 1.0;
-
     // Paramètres de la fenêtre de simulation.
     static final int CHAMBRE_LARGEUR = 100;
     static final int CHAMBRE_HAUTEUR = 100;
-    static final int HAUTEUR_MIN = 20;
+    static final int CHAMBRE_HAUTEUR_MIN = 20;
     static final int DELAI = 10;
     static final String WIN_TITRE = "Simulateur PVNRT - Groupe B5-2";
-    static final double DELTA_T_SIMULATION = 0.2;
+    static final double DELTA_T_SIMULATION = 0.1;
     static final double PISTON_VARIATION_MODULE = 0.1;
 
     // Constantes physiques.
-    static final double k = 2;
-    static final double kB = 1;
-    static final double Na = 1;
+    static final double k = 100;
+    static final double kB = 1.38;
+    static final double Na = 6.02;
     static final double R = 8.314;
+
+    // Paramètres des particules.
+    static final double MASSE_PARTICULE = 1;
+    static final int NOMBRE_PARTICULES = 100;
+    static final double VITESSE_INITIALE_PARTICULE = 2;
 
     // Différentes formules disponibles.
     enum Formule {
@@ -31,13 +31,15 @@ public class PVNRT {
     }
 
     // Commentaires.
-    static final String COMMENTAIRE_1 = "Algo observé : modèle V0 ressort aux parois.";
-    static final String COMMENTAIRE_2 = "PV=%f   nRT=%f";
+    static final String COMMENTAIRE_1 = "Algo observé : modèle V1.";
+    static final String COMMENTAIRE_2 = "PV=%f   nRT=%f   PV/nRT=%f";
 
     // Variables globales.
     static double[][] fParticules;
     static int[][] fCouleurs;
     static double fVitesseMax;
+    static double fVitessePiston;
+    static double fPistonAvant;
 
     /**
      * Initialise la simulation.
@@ -56,25 +58,31 @@ public class PVNRT {
             fCouleurs[i] = creerCouleurParticule(fParticules[i]);
 
         // Initialiser et afficher la fenêtre.
-	    Affichage.initAffichage(CHAMBRE_LARGEUR, CHAMBRE_HAUTEUR, HAUTEUR_MIN,
-	        DELAI, WIN_TITRE, DELTA_T_SIMULATION, PISTON_VARIATION_MODULE);
+        Affichage.initAffichage(CHAMBRE_LARGEUR, CHAMBRE_HAUTEUR,
+            CHAMBRE_HAUTEUR_MIN, DELAI, WIN_TITRE, DELTA_T_SIMULATION,
+            PISTON_VARIATION_MODULE);
         Affichage.setParticules(fParticules, fCouleurs);
         Affichage.setCommentaire(COMMENTAIRE_1);
+
+        // Position et vitesse initiale du piston.
+        fPistonAvant = Affichage.getPiston();
+        fVitessePiston = 0;
     }
 
     /**
      * Renvoie une nouvelle particule.
      * Génère aléatoirement une position et une vitesse initiale.
-     * La norme de la vitesse est toujours la même, mais l'angle initial est
-     * choisi aléatoirement dans [0; 2π[.
+     * La norme de la vitesse et l'angle initial sont choisis
+     * aléatoirement.
      */
     public static double[] creerParticule() {
+        double vitesse = VITESSE_INITIALE_PARTICULE * Math.random();
         double theta = Math.random() * 2 * Math.PI;
         double[] particule = {
             randomInRange(0, CHAMBRE_LARGEUR),             // Position X
-            randomInRange(0, HAUTEUR_MIN),                 // Position Y
-            VITESSE_INITIALE_PARTICULE * Math.cos(theta),  // Vitesse X
-            VITESSE_INITIALE_PARTICULE * Math.sin(theta),  // Vitesse Y
+            randomInRange(0, CHAMBRE_HAUTEUR_MIN),         // Position Y
+            vitesse * Math.cos(theta),                     // Vitesse X
+            vitesse * Math.sin(theta),                     // Vitesse Y
             0,                                             // Accélération X
             0                                              // Accélération Y
         };
@@ -86,7 +94,11 @@ public class PVNRT {
      */
     public static int[] creerCouleurParticule(double[] particule) {
         double rapport = calculVitesseParticule(particule) / fVitesseMax;
-        int[] couleur = {(int) (rapport * 255), 0, 0};
+        int[] couleur = {
+            (rapport > 0.6) ? (int) (rapport * 255) : 0,
+            (rapport >= 0.3 && rapport < 0.6) ? (int) (rapport * 255) : 0,
+            (rapport < 0.3) ? (int) ((1 - rapport) * 255) : 0
+        };
         return couleur;
     }
 
@@ -99,16 +111,27 @@ public class PVNRT {
     }
 
     /**
-     * Renvoie la norme de la force appliquée sur une particule (N).
+     * Renvoie les composantes de la force appliquée sur une particule.
      */
     public static double[] calculForceParticule(double[] particule) {
         double[] force = {0.0, 0.0};
-       
-        if (particule[1] <= 0 || particule[1] >= Affichage.getPiston())
-			force[1] = -2 * particule[3] * MASSE_PARTICULE / DELTA_T_SIMULATION;
-	    if (particule[0] <= 0 || particule[0] >= CHAMBRE_LARGEUR)
-			force[0] = -2 * particule[2] * MASSE_PARTICULE / DELTA_T_SIMULATION;
-			
+
+        // Haut
+        if (particule[1] < 0)
+            force[1] = -k * particule[1];
+
+        // Bas
+        else if (particule[1] > Affichage.getPiston())
+            force[1] = -k * (particule[1] - Affichage.getPiston());
+
+        // Gauche
+        if (particule[0] < 5)
+            force[0] = -k * particule[0];
+
+        // Droite
+        else if (particule[0] > CHAMBRE_LARGEUR)
+            force[0] = -k * (particule[0] - CHAMBRE_LARGEUR);
+            
         return force;
     }
 
@@ -117,17 +140,17 @@ public class PVNRT {
      * Pour calculer la pression instantanée, la moyenne des pressions
      * exercées sur chacun des 4 murs est calculée, elles-mêmes calculées en
      * sommant les forces exercées par chaque particule sur chaque mur et en
-	 * les divisant par la surface (la longueur du côté).
+     * les divisant par la surface (la longueur du côté).
      */
     public static double calculPressionMoyenneInstantanee() {
-		double sommeForce = 0.0;
-		double pressionMoyenne = 0.0;
-		double surfaceTotale = 0.0;
-		for (int i = 0; i < fParticules.length; i++)
-			sommeForce += norme(calculForceParticule(fParticules[i]));
-		surfaceTotale = 2 * CHAMBRE_LARGEUR + 2 * Affichage.getPiston();
-		pressionMoyenne = sommeForce / surfaceTotale;
-		
+        double sommeForce = 0.0;
+        double pressionMoyenne = 0.0;
+        double surfaceTotale = 0.0;
+        for (int i = 0; i < fParticules.length; i++)
+            sommeForce += norme(calculForceParticule(fParticules[i]));
+        surfaceTotale = 2 * CHAMBRE_LARGEUR + 2 * Affichage.getPiston();
+        pressionMoyenne = sommeForce / surfaceTotale;
+        
         return pressionMoyenne;
     }
 
@@ -143,11 +166,10 @@ public class PVNRT {
      * La température est calculée avec la formule du premier TD.
      */
     public static double calculTemperatureMoyenneInstantanee() {
-		double temperature = 0.0;
-		for(int i = 0; i<fParticules.length; i++) {
-			temperature += Math.pow(calculVitesseParticule(fParticules [i]), 2);
-		}
-		temperature *= MASSE_PARTICULE / (3 * kB);
+        double temperature = 0.0;
+        for(int i = 0; i < fParticules.length; i++)
+            temperature += Math.pow(calculVitesseParticule(fParticules [i]), 2);
+        temperature *= MASSE_PARTICULE / (3 * kB);
         return temperature;
     }
 
@@ -159,7 +181,7 @@ public class PVNRT {
     }
 
     /**
-     * Renvoie le produit P*V.
+     * Renvoie le produit PV.
      */
     public static double calculPV() {
         return calculPressionMoyenneInstantanee() * CHAMBRE_LARGEUR * Affichage.getPiston();
@@ -177,8 +199,8 @@ public class PVNRT {
      */
     public static void actualiserPositionParticule(int i) {
         double[] p = fParticules[i];
-        p[0] += p[2] * DELTA_T_SIMULATION;
-        p[1] += p[3] * DELTA_T_SIMULATION;
+        p[0] += p[2] * DELTA_T_SIMULATION + fVitessePiston * DELTA_T_SIMULATION;
+        p[1] += p[3] * DELTA_T_SIMULATION + fVitessePiston * DELTA_T_SIMULATION;
     }
 
     /**
@@ -208,11 +230,17 @@ public class PVNRT {
      */
     public static void next() {
         // Afficher les valeurs de PV et nRT.
-        Affichage.setCommentaire(String.format(COMMENTAIRE_2, calculPV(), calculnRT()));
+        Affichage.setCommentaire(String.format(COMMENTAIRE_2, calculPV(), calculnRT(), calculPV()/calculnRT()));
 
         // Déterminer la méthode en cours d'utilisation.
         Formule methode = Affichage.getCalcXYmethod().equals("EULER") ?
             Formule.Euler : Formule.Verlet;
+
+        // Mise à jour de la vitesse du piston.
+        double piston = Affichage.getPiston();
+        if (piston != CHAMBRE_HAUTEUR || piston != CHAMBRE_HAUTEUR_MIN)
+            fVitessePiston = (piston - fPistonAvant) / DELTA_T_SIMULATION;
+        fPistonAvant = piston;
 
         // Mise à jour des positions et des vitesses des particules.
         for (int i = 0; i < NOMBRE_PARTICULES; i++) {
@@ -235,11 +263,11 @@ public class PVNRT {
         return min + (int)(Math.random() * ((max - min) + 1));
     }
 
-	/**
-	 * Renvoie la norme d'une force.
-	 */
-	public static double norme(double[] force) {
-		return Math.sqrt(Math.pow(force[0], 2) + Math.pow(force[1], 2));
-	}
+    /**
+     * Renvoie la norme d'une force.
+     */
+    public static double norme(double[] force) {
+        return Math.sqrt(Math.pow(force[0], 2) + Math.pow(force[1], 2));
+    }
 
 }
